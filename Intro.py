559be261be3019f2dba097e10f1c3ac588fcde69
Viktor-JN <viktor.johanssonnygren@elev.ntig.se>
2023-08-24 14:39:54 +0200

    first commit

---
+++ Intro.py
@@ -0,0 +1,24 @@
+print("Hello world")
+print("Jag gillar inte apor \"Kung Carl XVI Gustaf\"")
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+print("Jag\nKung Carl XVI Gustaf\nanser att du luktar skit")
+print("Kung Carl XVI Gustaf" + " APA!")
+print("Drottningen Elizabeth ringde", "Kung Carl XVI Gustaf", "och frågade om han ville dricka te")
+svar=input("Hur många laxar finns det i laxasken?\n")
+print("Du gissade", svar, "och svaret var 6")
+print("Hej och välkommen till mitt program!\n" + "Jag heter Viktor och du heter?")
+namn=input()
+print("Hej", namn + "!")
+age=int(input("Hur gammal är du?\n"))
+print("Du är", age, "år gammal!")
